cd0ec2d670500f623fb13892539bfb02b50cd3af
Masny Ben <tyszko.piotrek.pt@gmail.com>
2023-01-20 15:20:07 +0100

    finished lesson 7

--- chapter-2/sandbox.js
+++ chapter-2/sandbox.js
@@ -1,5 +1,22 @@
-
+/*
  console.log(1);
  console.log(2);
  console.log(3);
+*/
+
+let age = 25;
+let year = 2019;
+
+//logs things to console
+console.log(age,year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var school = 99;
+console.log(school);
+
 
