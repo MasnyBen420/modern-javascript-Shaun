7e749829dfca057903e8f63928f7382539af4ed5
Masny Ben <tyszko.piotrek.pt@gmail.com>
2023-01-21 11:39:15 +0100

    finished lesson 15

---
+++ chapter-2/lesson 15/sandbox.js
@@ -0,0 +1,34 @@
+//booleans  & comparisons
+console.log(true,false,'true','false');
+
+// methods can return booleans
+let email = 'luigie@thenetninja.co.uk'
+let names = ['mario','luigie','toad']
+
+
+// let result = names.includes('!');
+
+// let result = email.includes('luigi');
+
+// console.log(result);
+
+
+//comparison operators
+
+let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 25);
+// console.log(age < 20);
+// console.log(age > 20);
+// console.log(age <= 25);
+// console.log(age >= 20);
+
+let name = 'shaun';
+
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+console.log(name > 'crystal');
+console.log(name > 'Shaun');
+console.log(name > 'Crystal');
