942ee2675fd5e4a80c996cf892b0caf1ef3854a2
Masny Ben <tyszko.piotrek.pt@gmail.com>
2023-01-21 12:12:15 +0100

    finished chapter 2

---
+++ chapter-2/lesson 17/sandbox.js
@@ -0,0 +1,18 @@
+// type conversion
+
+let score = '100';
+
+// score = Number(score);
+// console.log(score + 1 );
+// console.log(typeof score);
+
+// let result = Number('hello');
+// let result = String(123);
+// let result = Boolean(-1);
+
+
+// let result = Boolean('');
+let result = Boolean('abc')
+
+console.log(result, typeof result);
+
