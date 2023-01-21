d9046efb07c845200efcf2bf1cb11da064ffaec8
Masny Ben <tyszko.piotrek.pt@gmail.com>
2023-01-21 11:44:09 +0100

    finished lesson 16

---
+++ chapter-2/lesson 16/sandbox.js
@@ -0,0 +1,16 @@
+let age = 25;
+
+// loose comparison (differnt types can still be equal)
+
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log(age != '25');
+
+//strict comparison (differnt types cannot be equal)
+
+
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25');
