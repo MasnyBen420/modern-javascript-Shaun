e42d595410442f65017edc27ee6b7fc89bc448a1
Masny Ben <tyszko.piotrek.pt@gmail.com>
2023-01-23 16:15:39 +0100

    finished lesson 26

---
+++ chapter-3/lesson 26/sandbox.js
@@ -0,0 +1,17 @@
+// break and continue
+
+const score = [50,25,0,30,100,20,10];
+
+
+for (let i=0;i < score.length;i++){
+    if(score[i]=== 0){
+        continue
+    }
+
+    console.log('your score : ',score[i]);
+
+    if(score[i]=== 100){
+        console.log('congratsyou got the top score');
+        break;
+    }
+}
