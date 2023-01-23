3d82ee85ecb5928a6f64b982d7e6b5f249fa173d
Masny Ben <tyszko.piotrek.pt@gmail.com>
2023-01-23 17:23:23 +0100

    finished lesson 32

---
+++ chapter-4/lesson 32/sandbox.js
@@ -0,0 +1,13 @@
+const calcArea = function(radius){
+    let area= Math.round(Math.PI,2) * radius**2;
+    return(area);
+}
+
+
+const area = calcArea(5);
+console.log(area);
+
+const calcVol = function(area){
+
+};
+calcVol(area)
