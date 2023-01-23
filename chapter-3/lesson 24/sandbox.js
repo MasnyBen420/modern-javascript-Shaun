df446c53a5a70244592b8ba60ad8569cf25ce3dd
Masny Ben <tyszko.piotrek.pt@gmail.com>
2023-01-23 16:05:05 +0100

    finished lesson 24

---
+++ chapter-3/lesson 24/sandbox.js
@@ -0,0 +1,9 @@
+//logical operators - OR || and AND &&
+
+const password = 'password1234'; 
+if(password.length >= 12 && password.includes('@') ){
+console.log('that password is mighty strong'); 
+} else if(password.length>=8 || password.includes('*')){
+console.log('that password is long enough!'); 
+} else { 
+console.log('password is not long enough'); } 
