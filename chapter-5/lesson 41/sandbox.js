9757bb0f768c44879cb2b9cbd9cf296a54f3d087
Masny Ben <tyszko.piotrek.pt@gmail.com>
2023-01-24 17:22:31 +0100

    finished lesson 41

--- chapter-5/lesson 41/sandbox.js
+++ chapter-5/lesson 41/sandbox.js
@@ -1,9 +1,19 @@
+const blogs=[
+    {title: 'why mac and cheese rules',likes:'30'},
+    {title: '10 things to make with marmite',likes:'70'}
+]
+
+
+
 let user = {
     name:'crystal',
     age:30,
     email: 'crystal@thenetninja.co.uk',
     location:'berlin',
-    blogs:['why mac and cheese rules','10 things to make with marmite'],
+    blogs:[
+        {title: 'why mac and cheese rules',likes:'30'},
+    {title: '10 things to make with marmite',likes:'70'}
+    ],
 
     login(){
         console.log('user logged in');
@@ -15,12 +25,12 @@ let user = {
         // console.log(this.blogs );
         console.log('this user has written the following blogs');
         this.blogs.forEach(blog => {
-            console.log(blog);
+            console.log(blog.title,blog.likes);
           })
         }
 };
 
 user.logBlogs();
-
+console.log(blogs);
 
 
