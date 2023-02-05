2369d029a3bc693a606c5376847defbc0162f597
Masny Ben <tyszko.piotrek.pt@gmail.com>
2023-02-05 13:26:08 +0100

    finished lesson 49

---
+++ chapter-6/lesson 49/sandbox.js
@@ -0,0 +1,11 @@
+const link = document.querySelector('a')
+
+
+link.setAttribute('href','https://www.netninja.co.uk')
+link.innerText=('The Net ninja website')
+
+const mssg = document.querySelector("p");
+
+mssg.setAttribute('class','success')
+
+mssg.setAttribute('style','color:green')
