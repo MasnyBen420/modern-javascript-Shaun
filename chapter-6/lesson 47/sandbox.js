3f2801f93e03a282931fcf83afd50324e7cc0a91
Masny Ben <tyszko.piotrek.pt@gmail.com>
2023-02-05 12:39:01 +0100

    finished lesson 47

---
+++ chapter-6/lesson 47/sandbox.js
@@ -0,0 +1,16 @@
+
+// get an element by ID
+
+ const title = document.getElementById('page-title')
+ console.log(title);
+
+//get elements by thier class name
+
+ const errors = document.getElementsByClassName('error')
+ console.log(errors[2]);
+
+// get elements by thier tag name 
+
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
